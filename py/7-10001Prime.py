55119b250336219befbb13b155e28974e286c433
BigBrainTime <57951435+BigBrainTime@users.noreply.github.com>
2022-05-01 03:53:01 -0500

    Update 7-10001Prime.py
    made it run a bit faster by using previously found primes

--- py/7-10001Prime.py
+++ py/7-10001Prime.py
@@ -1,17 +1,30 @@
 prime = 0
+primes = []
 n = 2
 
 while prime < 10002: #one extra bc for some reason 4 is counted as a prime
     boolcheck = []
 
-    for i in range(2,n//2):
-        if n % i == 0:
-            #divideable
-            boolcheck.append(False)
-            break
+    if len(primes) <= 10:
+        for i in range(2,n//2):
+            if n % i == 0:
+                #divideable
+                boolcheck.append(False)
+                break
 
-        else:
-            boolcheck.append(True)
+            else:
+                boolcheck.append(True)
+
+    else:
+        for i in primes:
+            if n % i == 0:
+                #divideable
+                boolcheck.append(False)
+                break
+
+            else:
+                boolcheck.append(True)
+
 
     if False not in boolcheck:
         prime += 1
